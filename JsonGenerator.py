edb73dce0c2d5718430ddae6fe3cb703ca3feb64
ANewDeviloper <teufel.timo6@gmail.com>
2016-10-15 17:14:49 +0200

    Json Aktualisierung

--- JsonGenerator.py
+++ JsonGenerator.py
@@ -6,14 +6,14 @@ def generateJsonDatei(Aufgabentyp,Aufgabe,Loesungen,Tipp,Schritte = None):
     i = 0
     Loesung = []
     while(len(Loesungen) > i):
-        Loesung.append(Loesungen[i])
+        Loesung.append(str(Loesungen[i]))
         i = i + 1
     
     return {"Physik_Pendel" :
-            json.dumps({'Aufgabentyp': Aufgabentyp,'Aufgabe': Aufgabe,'Tipp': Tipp,'Loesung': Loesungen}),
+            json.dumps({'Aufgabentyp': Aufgabentyp,'Aufgabe': Aufgabe,'Tipp': Tipp,'Loesung': Loesung}),
             
             "Polynom_Nullstellen" :
-            json.dumps({'Aufgabentyp': Aufgabentyp,'Aufgabe': Aufgabe,'Tipp': Tipp,'Schritte': Schritte,'Loesung': ["1","2","3"]})}.get(Aufgabentyp)
+            json.dumps({'Aufgabentyp': Aufgabentyp,'Aufgabe': Aufgabe,'Tipp': Tipp,'Schritte': Schritte,'Loesung': Loesung})}.get(Aufgabentyp)
      
 
 print(generateJsonDatei("Polynom_Nullstellen","Aufgabe",[1,2,3],"Tipp"))
