a7765d78eab623d9a942580a8189630468397542
paalwi <paulawiesner@gmx.net>
2016-10-15 14:58:09 +0200

    Pendel Physik Uebungen-Generator

---
+++ trigonometrischeFunktionen.py
@@ -0,0 +1,37 @@
+#Aufgabe: momentane Auslenkung berechnen bei gegebener Fraquenz, Zeit, max. Auslenkung
+import random
+import math
+
+
+class TrigonometriePendel(object):
+
+    def __init__(self):
+        self.typ = "Physik_Pendel"
+        self.phase = 0
+        self.frequenz = 0
+        self.amplitude = 0
+        self.tipp = "Die Formel = s(t) = amplitude * sin(2 * pi * frequenz + phase)"
+
+    def aufgabenText(self):
+        return "Berechne die momentane Auslenkung des Fadenpendels, max. Auslenkung: %d, Frequenz %d, Phase %d"\
+            %(self.amplitude, self.frequenz, self.phase)
+
+    def aufgabeErstellen(self):
+        self.phase = random.randrange(0, 6)
+        self.frequenz = random.randrange(1, 4)
+        self.amplitude = random.randrange(2, 5)
+
+    def tipp(self):
+        return self.tipp
+
+
+    def loesung(self):
+        return round(self.amplitude * math.sin(2 * math.pi * self.frequenz + self.phase), 2)
+
+
+
+test = TrigonometriePendel()
+test.aufgabeErstellen()
+print(test.phase)
+print(test.aufgabenText())
+print(test.loesung())
