11d01f76f9f4a2884d28293a7d0163fcd3b636cf
Leo Maroni <leo@labcode.de>
2016-10-15 20:30:57 +0200

    If

--- assets/js/aufgabe/send.js
+++ assets/js/aufgabe/send.js
@@ -1,12 +1,18 @@
 $('#sentbtn').click(function() {
   var entered = $('#text').val()
-  if(entered == localStorage.getItem('loesung'))
-
+  if(entered == localStorage.getItem('loesung')) {
   $('#start').hide()
   //NOT COOL >
   $('#sent').show()
   // < TO DO THIS
   $('#solving_sent').attr("value", entered)
+  } else {
+    localStorage.setItem('step', 0)
+    $('#stepsolving_schrittid').text(localStorage.getItem('step')+1)
+    $('#start').hide()
+    $('#stepsolving').show()
+    $('#stepsolving_wrong').show()
+  }
 })
 $('#nextexercisebtn').click(function () {
   $("#sent").hide()
