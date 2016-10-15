d7d416eb4bf339c18df28b83f99fb657b8ded9fe
Leo Maroni <leo@labcode.de>
2016-10-15 15:10:31 +0200

    #2 4rd

---
+++ assets/js/aufgabe/stepsolving.js
@@ -0,0 +1,16 @@
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+$('#stepsolvingbtn').click(function () {
+  var step = 1
+  $('#stepsolving_schrittid').text(step)
+  $('#start').hide()
+  $('#stepsolving').show()
+})
