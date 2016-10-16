cd9b6e0327b385d083c110fbc909f6f8624cc8a7
ANewDeviloper <teufel.timo6@gmail.com>
2016-10-16 09:59:30 +0200

    Zahlendarstellung verbessert

--- Zahlendarstellung.py
+++ Zahlendarstellung.py
@@ -14,38 +14,56 @@ def erstelleAufgabe(dezimal, basis):
     return {1 : ("Stellen sie die Zahl " + str(dezimal) + " zur Basis " + str(basis) + " dar.")}.get(1)
 
 def loeseAufgabe(dezimal, basis):
-    Ergebnis = "";
-    ZahlArray = []
-
-    while(dezimal >= basis):
+    check = True
+    while(check):
+        check = False;
+        Ergebnis = "";
+        ZahlArray = []
+        c = 0;
+        while(dezimal >= basis):
+            ZahlArray.append(dezimal % basis)
+            dezimal = (dezimal-(dezimal % basis))/basis
+            if c > 10:
+                check = True
+                break
         ZahlArray.append(dezimal % basis)
-        dezimal = (dezimal-(dezimal % basis))/basis
         
-    ZahlArray.append(dezimal % basis)
-
     ZahlArray.reverse()
     i = 0
-
     while(i < len(ZahlArray)):
         Ergebnis += str(ZahlArray[i])
         i+=1
-
     return int(Ergebnis)
 
 def erzeugeSchritte(dezimal, basis):
-    SchritteArray = []
-    while(dezimal >= basis):
-        ZahlArray.append(dezimal % basis)
-        dezimal = (dezimal-(dezimal % basis))/basis
+    check = True
+    while(check):
+        modulo = 0;
+        check = False;
+        SchritteArray = []
+        c = 0;
+        while(dezimal >= basis):
+            oldDezimal = dezimal
+            modulo = dezimal % basis
+            dezimal = (dezimal-(dezimal % basis))/basis
+            SchritteArray.append(str(oldDezimal) + " % " + str(basis) + " = " + str(dezimal) + " Rest: " + str(modulo))
+            if c > 10:
+                check = True
+                break
+        oldDezimal = dezimal
+        modulo = dezimal % basis
+        SchritteArray.append(str(oldDezimal) + " % " + str(basis) + " = " + str(dezimal) + " Rest: " + str(modulo))
+    return SchritteArray
         
-def anJson(Aufgabentyp, Aufgabe, Loesung, Tipp):
-    return JsonGenerator.generateJsonDatei(Aufgabentyp, Aufgabe, [Loesung], Tipp)
+def anJson(Aufgabentyp, Aufgabe, Loesung, Tipp, Schritte):
+    return JsonGenerator.generateJsonDatei(Aufgabentyp, Aufgabe, [Loesung], Tipp, Schritte)
 
 
 Aufgabe = erstelleAufgabe(Dezimal, Basis)
 Loesung = loeseAufgabe(Dezimal, Basis)
+Schritte = erzeugeSchritte(Dezimal, Basis)
 
-print(anJson(Aufgabentyp, Aufgabe, Loesung, Tipp))
+print(anJson(Aufgabentyp, Aufgabe, Loesung, Tipp, Schritte))
 
 
 
